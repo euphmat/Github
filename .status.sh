144c79bb4a4a2e5fac7b5882b968e2aff8ad2e1d
euphmat <euphmat@gmail.com>
2020-08-09 21:56:39 +0900

    status column

--- .status.sh
+++ .status.sh
@@ -1,46 +1,52 @@
 #!/bin/bash
 
-# ===========================================
-# Github/
-# ===========================================
-cd ~/Github
-files=~/Github/*
-echo -en "\033[0;34mGithub\033[0;39m "
-if [[ -d ./.git/ ]]; then
-  if [ -z "$(git status --porcelain)" ]; then
-    echo -en "\033[0;32mClean\033[0;39m"
-    echo -en "`git branch`" | grep -E "\*" | sed -e "s/\*//"
+function get_directory() {
+  files=~/Github/*
+  dirary=()
+  for filepath in $files; do
+    if [ -d $filepath ] ; then
+      dirary+=("$filepath")
+    fi
+  done
+}
+
+function print_repository_name(){
+  reponame=`echo $i | awk -F '/' '{print $(NF)}'`
+  echo -en "\033[0;34m$reponame\033[0;39m" 
+}
+
+function print_git_status() {
+  branchname=`git branch | grep -E "\*" | sed -e "s/\*//"`
+  if [[ -d ./.git/ ]]; then
+    if [ -z "$(git status --porcelain)" ]; then
+      echo -en "$branchname"
+      echo -e " \033[0;32mClean\033[0;39m"
+    else
+      echo -en "$branchname"
+      echo -e " \033[0;31mUncommittedChanges\033[0;39m"
+    fi
   else
-    echo -en "\033[0;31mUncommittedChanges\033[0;39m"
-    echo -en "`git branch`" | grep -E "\*" | sed -e "s/\*//"
+    echo -en "$branchname"
+    echo -e " \033[0;33mnot Git Repository\033[0;39m"
   fi
-fi
+}
 
-# ===========================================
-# Github/* 
-# ===========================================
-dirary=()
-for filepath in $files; do
-  if [ -d $filepath ] ; then
-    dirary+=("$filepath")
-  fi
-done
+# ================================================
+# main
+# ================================================
+function main(){
+  echo -en "\033[0;34mGithub\033[0;39m" 
+  # Github/
+  cd ~/Github; 
+  print_git_status
 
+  # Github/*
+  get_directory
+  for i in ${dirary[@]}; do
+    cd $i
+    print_repository_name
+    print_git_status
+  done
+}
 
-for i in ${dirary[@]}; do
-reponame=`echo $i | awk -F '/' '{print $(NF)}'`
-echo -en "\033[0;34m$reponame\033[0;39m " 
-cd $i
-if [[ -d ./.git/ ]]; then
-if [ -z "$(git status --porcelain)" ]; then
-echo -en "\033[0;32mClean\033[0;39m"
-echo -en "`git branch`" | grep -E "\*" | sed -e "s/\*//"
-else
-echo -en "\033[0;31mUncommitted Changes\033[0;39m"
-echo -en "`git branch`" | grep -E "\*" | sed -e "s/\*//"
-fi
-else
-echo -e "\033[0;33mnot Git Repository\033[0;39m"
-fi
-done
-
+main | column -t
