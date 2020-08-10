2124871f79323c77390b8a18aff6a2c85830f643
euphmat <euphmat@gmail.com>
2020-08-10 18:24:11 +0900

    update init.sh

--- .init.sh
+++ .init.sh
@@ -1,51 +1,58 @@
 #!/bin/bash
 set -Ceu
 
-# =====================================================================================
-# install Homebrew
-# =====================================================================================
-if [[ `type brew > /dev/null 2>&1; echo $?` = 0 ]]; then
-        echo "Homebrew Already Installed."
-else
+function install_homebrew(){
+    if [[ `type brew > /dev/null 2>&1; echo $?` = 0 ]]; then
+        echo "✅ Homebrew Already Installed."
+    else
         echo "installing Homebrew..."
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-fi
+    fi
+}
 
-# =====================================================================================
-# install Git
-# =====================================================================================
-if [[ `type git > /dev/null 2>&1; echo $?` = 0 ]]; then
-        echo "Git Already Installed."
-else
+function install_git(){
+    if [[ `type git > /dev/null 2>&1; echo $?` = 0 ]]; then
+        echo "✅ Git Already Installed."
+    else
         echo "installing Git..."
         brew install git
-fi
+    fi
+}
 
-# =====================================================================================
-# Check Github Directory && git Clone 
-# =====================================================================================
-cd ~
-if [[ -d ./Github/ ]]; then
-        echo "The Github directory already exists."
-else
+function install_github_repository(){
+    cd ~
+    if [[ -d ./Github/ ]]; then
+        echo "✅ The Github directory already exists."
+    else
         git clone https://github.com/euphmat/Github
-        cd Github/
-        curl https://github.com/euphmat?tab=repositories | grep -E '.*<a href="\/euphmat\/.*"' | sed -e "s/^.*euphmat\/// "| sed -e "s/\" itemprop=\"name codeRepository\" >//" | sed -e "s/Github//" > repo.txt
+        cd ~/Github/
+        curl https://github.com/euphmat?tab=repositories | grep -E '.*<a href="\/euphmat\/.*"' >| repo.txt
+        sed -i -e "s/^.*euphmat\/// " repo.txt
+        sed -i -e "s/\" itemprop=\"name codeRepository\" >//" repo.txt
+        sed -i -e "s/Github//" repo.txt
+        sed -i -e "1d" repo.txt
         data_source=./repo.txt
         while read line
         do
-                repository_name=`echo $line`
-                git clone https://github.com/euphmat/$repository_name
-
+            repository_name=`echo $line`
+            `git clone https://github.com/euphmat/$repository_name`
         done < $data_source
-fi
-rm repo.txt
+        rm repo.txt
+    fi
+}
 
-# =====================================================================================
-# dotfiles init
-# =====================================================================================
-cd ~/github/dotfiles/bin
-./defaults.sh
-./install_package.sh
-./setting_tool.sh
-./deploy.sh
+function dotscript(){
+    cd ~/github/dotfiles/bin
+    ./defaults.sh
+    ./install_package.sh
+    ./setting_tool.sh
+    ./deploy.sh
+}
+
+function main(){
+    install_homebrew
+    install_git
+    install_github_repository
+    dotscript
+}
+main
