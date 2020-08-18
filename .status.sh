7b6450bac4c77ce6c47eea333018358550dd3807
euphmat <euphmat@gmail.com>
2020-08-18 15:54:58 +0900

    enhanceted git remote show

--- .status.sh
+++ .status.sh
@@ -3,16 +3,20 @@ set -Ceuo pipefail
 
 function main(){
   # Github/
+  check_github
   echo -en "\033[0;34mGithub\033[0;39m" 
   cd ~/Github; 
   print_git_status
+  echo ""
 
   # Github/*
   get_directory
   for i in ${dirary[@]}; do
     cd $i
+    check_github
     print_repository_name
     print_git_status
+    echo ""
   done
 }
 
@@ -36,15 +40,24 @@ function print_git_status(){
   if [[ -d ./.git/ ]]; then
     if [ -z "$(git status --porcelain)" ]; then
       echo -en "$branchname"
-      echo -e " \033[0;32mClean\033[0;39m"
+      echo -en " \033[0;32mClean\033[0;39m"
     else
       echo -en "$branchname"
-      echo -e " \033[0;31mUncommittedChanges\033[0;39m"
+      echo -en " \033[0;31mUncommittedChanges\033[0;39m"
     fi
   else
     echo -en "$branchname"
-    echo -e " \033[0;33mnotGitRepository\033[0;39m"
+    echo -en " \033[0;33mnotGitRepository\033[0;39m"
   fi
+}
+
+function check_github(){
+        check_result=`git remote show origin | tail -1 | sed -e 's/.* master //g'` 
+        if [[ $check_result = "(local out of date)" ]]; then
+                echo -en "[\033[0;31m!\033[0;39m] "
+        else
+                echo -en "[\033[0;32mo\033[0;39m] "
+        fi
 }
 
 main | column -t
