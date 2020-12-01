e0e76d457d4ecbd36ab3e381d4af5167ba76d861
euphmat <euphmat@gmail.com>
2020-12-01 09:33:42 +0900

    update pull

--- .bin/pull.sh
+++ .bin/pull.sh
@@ -1,18 +1,46 @@
 #!/bin/bash
 
 function main(){
-        # Github管理リポジトリに対して`git pull`
+        # Github
+        # ========================================================
         cd ~/Github
-        echoo Github
+        print blank
+        print Github
         pull_current_directory
 
-        # Githubディレクトリ配下のリポジトリに対して`git pull`
+        # Github 配下
+        # ========================================================
         store_repository_name_to_array
         for i in ${dirary[@]}; do
                 cd $i
-                echoo Repository
+                print Repository
                 pull_current_directory
         done
+}
+
+function pull_current_directory() {
+        if [[ -d ./.git/ ]]; then
+                if [[ `git pull > /dev/null 2>&1 ; echo $?` = 0 ]]; then
+                        print Branch && print Success
+                else
+                        print Branch && print Failed
+                fi
+        else
+                print Branch && print not_Git_Repository
+        fi
+}
+
+function print(){
+        case $1 in
+                "Github" ) echo -en "\033[0;34mGithub\033[0;39m" ;;
+                "Repository" ) echo -en "\033[0;34m`echo $i | awk -F '/' '{print $(NF)}'`\033[0;39m" ;;
+                "Branch" ) echo -en "`git branch | grep -E "\*" | sed -e "s/\*//"`" ;;
+                "Success" ) echo -e " \033[0;32mSuccessfull\033[0;39m" ;;
+                "Failed" ) echo -e " \033[0;31mFailed\033[0;39m" ;;
+                "not_Git_Repository" ) echo -e " \033[0;33mnotGitRepository\033[0;39m" ;;
+                "line" ) echo "========================" ;;
+                "blank" ) echo "" ;;
+        esac
 }
 
 function store_repository_name_to_array() {
@@ -23,29 +51,6 @@ function store_repository_name_to_array() {
                         dirary+=("$filepath")
                 fi
         done
-}
-
-function pull_current_directory() {
-        if [[ -d ./.git/ ]]; then
-                if [[ `git pull > /dev/null 2>&1 ; echo $?` = 0 ]]; then
-                        echoo Branch && echoo Success
-                else
-                        echoo Branch && echoo Failed
-                fi
-        else
-                echoo Branch && echoo not_Git_Repository
-        fi
-}
-
-function echoo(){
-        case $1 in
-                "Github" ) echo -en "\033[0;34mGithub\033[0;39m" ;;
-                "Repository" ) echo -en "\033[0;34m`echo $i | awk -F '/' '{print $(NF)}'`\033[0;39m" ;;
-                "Branch" ) echo -en "`git branch | grep -E "\*" | sed -e "s/\*//"`" ;;
-                "Success" ) echo -e " \033[0;32mSuccessfull\033[0;39m" ;;
-                "Failed" ) echo -e " \033[0;31mFailed\033[0;39m" ;;
-                "not_Git_Repository" ) echo -e " \033[0;33mnotGitRepository\033[0;39m" ;;
-        esac
 }
 
 main
